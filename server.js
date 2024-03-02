9f4a5e14de423e95bbe598b571f8afc940e124ef
Nathan-bit <gabiamsamuelnathan@gmail.com>
2024-03-03 00:30:08 +0100

    ejs test

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.set('view engine', 'ejs');
+app.set('views','./views')
+app.use(express.static(__dirname + '/public'));
 
+let message ="je suis une message"
+app.get('/', (req, res) =>{
+  res.render('index',{message: message});
+})
+
+app.get('/header' , (req, res) => {
+    res.render('partiales/header')
+  })
+app.get('/footer', (req, res) => {res.render('partiales/footer')})
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
